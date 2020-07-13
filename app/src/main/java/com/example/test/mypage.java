9e5bdec1bf4d60eb26d47c16b3b28e4d5fe89a75
minji4928 <minji4928@naver.com>
2020-07-13 16:33:21 +0900

    메인 틀 잡기, mypage 나가기버튼, 바로가기 틀, 날씨까지

--- app/src/main/java/com/example/test/mypage.java
+++ app/src/main/java/com/example/test/mypage.java
@@ -1,16 +1,32 @@
 package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 public class mypage extends AppCompatActivity {
-
+    ImageButton exitbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        exitbutton = findViewById(R.id.exitbutton);
+        exitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                overridePendingTransition(R.anim.hold, R.anim.mypage2);
+                finish();
+            }
+        });
+
     }
 
     @Override
@@ -19,4 +35,5 @@ public class mypage extends AppCompatActivity {
         overridePendingTransition(R.anim.hold,R.anim.mypage2);
 
     }
+
 }
